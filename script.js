eb0793ef3e1005e69d58e0dca55fcca441c29d90
Hunter Padgett <gizmokabanjo@gmail.com>
2022-11-08 16:01:16 -0500

    desktop doge

---
+++ script.js
@@ -0,0 +1,20 @@
+const bodyFloat = document.querySelector('body');
+
+bodyFloat.addEventListener('mousemove', (event) => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  const spanElement = document.createElement('span');
+  spanElement.style.left = x + 'px';
+  spanElement.style.top = y + 'px';
+  const size = randomNumber() + 'px';
+  spanElement.style.width = size;
+  spanElement.style.height = size;
+  bodyFloat.appendChild(spanElement);
+  setTimeout(() => {
+    spanElement.remove()
+  }, 3000)
+})
+
+function randomNumber() {
+  return Math.floor(Math.random() * 100)
+}
