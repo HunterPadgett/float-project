d33aaec2acc8423d57ec594fc7d87ab69c4e841f
Hunter Padgett <gizmokabanjo@gmail.com>
2022-11-09 11:36:23 -0500

    party audio

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bodyFloat = document.querySelector('body');
+const partyMusic = document.querySelector('audio');
 
 bodyFloat.addEventListener('mousemove', (event) => {
   const x = event.offsetX;
@@ -12,11 +13,11 @@ bodyFloat.addEventListener('mousemove', (event) => {
   bodyFloat.appendChild(spanElement);
   setTimeout(() => {
     spanElement.remove()
-  }, 3000)
+  }, 100000)
 })
+
+partyMusic.volume = 0.1
 
 function randomNumber() {
   return Math.floor(Math.random() * 100)
-  
 }
-console.log(Math.floor(Math.random() * 100))
