257f768beff3d57b5b4e1a9d22c9305eec5defa0
Hunter Padgett <gizmokabanjo@gmail.com>
2022-11-09 15:58:26 -0500

    mobile audio autoplay

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const bodyFloat = document.querySelector('body');
-const partyMusic = document.querySelector('audio');
+const bodyFloat = document.querySelector("body");
+const partyMusic = document.querySelector("audio");
 
-document.addEventListener("touchstart", function(){}, true);
+document.addEventListener("touchstart", function () {}, true);
 
-bodyFloat.addEventListener('mousemove', (event) => {
-  const x = event.offsetX;
-  const y = event.offsetY;
-  const spanElement = document.createElement('span');
-  spanElement.style.left = x + 'px';
-  spanElement.style.top = y + 'px';
-  const size = randomNumber() + 'px';
-  spanElement.style.width = size;
-  spanElement.style.height = size;
-  bodyFloat.appendChild(spanElement);
-  setTimeout(() => {
-    spanElement.remove()
-  }, 10000)
-  event.preventDefault();
-})
+bodyFloat.addEventListener("mousemove", (event) => {
+ const x = event.offsetX;
+ const y = event.offsetY;
+ const spanElement = document.createElement("span");
+ spanElement.style.left = x + "px";
+ spanElement.style.top = y + "px";
+ const size = randomNumber() + "px";
+ spanElement.style.width = size;
+ spanElement.style.height = size;
+ bodyFloat.appendChild(spanElement);
+ setTimeout(() => {
+  spanElement.remove();
+ }, 10000);
+ 
+});
 
 const volume = () => {
-  partyMusic.volume = 0.1;
-  partyMusic.autoplay;
-  partyMusic.loop;
-}
+ partyMusic.volume = 0.1;
+ partyMusic.autoplay;
+ partyMusic.loop;
+};
 
 function randomNumber() {
-  return Math.floor(Math.random() * 100)
+ return Math.floor(Math.random() * 100);
 }
 
 volume();
